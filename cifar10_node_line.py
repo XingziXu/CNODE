308a93f677a63d2c99b5f8cc1cd876ad212c2dd3
Xingzi Xu <xingzi.xu@duke.edu>
2021-08-21 10:06:51 -0400

    latest

--- cifar10_node_line.py
+++ cifar10_node_line.py
@@ -20,13 +20,13 @@ class Grad_net(nn.Module): # the Grad_net defines the networks for the path and 
 
         self.path = nn.Sequential( # define the network for the integration path
         nn.Conv2d(4,width_path,1,1,0),
-        nn.Softsign(),
+        nn.ReLU(),
         nn.Conv2d(width_path,width_path,3,1,1),
-        nn.Softsign(),
+        nn.ReLU(),
         nn.Conv2d(width_path,3,1,1,0),
         nn.Flatten(),
         nn.Linear(3072,3),
-        #nn.PReLU()
+        nn.PReLU()
         )
         
         self.grad_g = nn.Sequential( # define the network for the gradient on x direction
