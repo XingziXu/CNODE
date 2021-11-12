5247c23347987697b07b8e3e56e2226f08d453d7
XingziXu <85845221+XingziXu@users.noreply.github.com>
2021-11-12 14:19:39 -0500

    latest

--- vis_adjoint.py
+++ vis_adjoint.py
@@ -31,99 +31,100 @@ cifar_line1 = 2.3
 
 
 length = 40
+epochs = np.linspace(0,length,length)
 
-accu1 = np.append(svhn_line,np.load('accuracy_svhn_line1.npy'))
-accu2 = np.append(svhn_line,np.load('accuracy_svhn_line2.npy'))
-accu3 = np.append(svhn_line,np.load('accuracy_svhn_line3.npy'))
-accu4 = np.append(svhn_line,np.load('accuracy_svhn_line4.npy'))
-accu5 = np.append(svhn_line,np.load('accuracy_svhn_line5.npy'))
+accu1 = np.append(svhn_line,np.load('accuracy_svhn_line_adj2.npy'))
+accu2 = np.append(svhn_line,np.load('accuracy_svhn_line_adj3.npy'))
+accu3 = np.append(svhn_line,np.load('accuracy_svhn_line_adj4.npy'))
+accu4 = np.append(svhn_line,np.load('accuracy_svhn_line_adj7.npy'))
+accu5 = np.append(svhn_line,np.load('accuracy_svhn_line_adj8.npy'))
 
 min_accu = np.zeros((length,1))
 max_accu = np.zeros((length,1))
 ave_accu = np.zeros((length,1))
 
 for i in range(0,length):
-    min_accu[i] = min([accu1[i],accu2[i],accu3[i],accu4[i],accu5[i]])
-    max_accu[i] = max([accu1[i],accu2[i],accu3[i],accu4[i],accu5[i]])
-    ave_accu[i] = average([accu1[i],accu2[i],accu3[i],accu4[i],accu5[i]])
+    min_accu[i] = min([accu1[i],accu2[i],accu4[i],accu5[i]])
+    max_accu[i] = max([accu1[i],accu2[i],accu4[i],accu5[i]])
+    ave_accu[i] = average([accu1[i],accu2[i],accu4[i],accu5[i]])
 
-test1 = np.append(svhn_line1,np.load('test_loss_svhn_line1.npy'))
-test2 = np.append(svhn_line1,np.load('test_loss_svhn_line2.npy'))
-test3 = np.append(svhn_line1,np.load('test_loss_svhn_line3.npy'))
-test4 = np.append(svhn_line1,np.load('test_loss_svhn_line4.npy'))
-test5 = np.append(svhn_line1,np.load('test_loss_svhn_line5.npy'))
+test1 = np.append(svhn_line1,np.load('test_loss_svhn_line_adj2.npy'))
+test2 = np.append(svhn_line1,np.load('test_loss_svhn_line_adj3.npy'))
+test3 = np.append(svhn_line1,np.load('test_loss_svhn_line_adj4.npy'))
+test4 = np.append(svhn_line1,np.load('test_loss_svhn_line_adj7.npy'))
+test5 = np.append(svhn_line1,np.load('test_loss_svhn_line_adj8.npy'))
 
 min_test = np.zeros((length,1))
 max_test = np.zeros((length,1))
 ave_test = np.zeros((length,1))
 
 for i in range(0,length):
-    min_test[i] = min([test1[i],test2[i],test3[i],test4[i],test5[i]])
-    max_test[i] = max([test1[i],test2[i],test3[i],test4[i],test5[i]])
-    ave_test[i] = average([test1[i],test2[i],test3[i],test4[i],test5[i]])
+    min_test[i] = min([test1[i],test2[i],test4[i],test5[i]])
+    max_test[i] = max([test1[i],test2[i],test4[i],test5[i]])
+    ave_test[i] = average([test1[i],test2[i],test4[i],test5[i]])
 
-train1 = np.append(svhn_line1,np.load('train_loss_svhn_line1.npy'))
-train2 = np.append(svhn_line1,np.load('train_loss_svhn_line2.npy'))
-train3 = np.append(svhn_line1,np.load('train_loss_svhn_line3.npy'))
-train4 = np.append(svhn_line1,np.load('train_loss_svhn_line4.npy'))
-train5 = np.append(svhn_line1,np.load('train_loss_svhn_line5.npy'))
+train1 = np.append(svhn_line1,np.load('train_loss_svhn_line_adj2.npy'))
+train2 = np.append(svhn_line1,np.load('train_loss_svhn_line_adj3.npy'))
+train3 = np.append(svhn_line1,np.load('train_loss_svhn_line_adj4.npy'))
+train4 = np.append(svhn_line1,np.load('train_loss_svhn_line_adj7.npy'))
+train5 = np.append(svhn_line1,np.load('train_loss_svhn_line_adj8.npy'))
 
 min_train = np.zeros((length,1))
 max_train = np.zeros((length,1))
 ave_train = np.zeros((length,1))
 
 for i in range(0,length):
-    min_train[i] = min([train1[i],train2[i],train3[i],train4[i],train5[i]])
-    max_train[i] = max([train1[i],train2[i],train3[i],train4[i],train5[i]])
-    ave_train[i] = average([train1[i],train2[i],train3[i],train4[i],train5[i]])
+    min_train[i] = min([train1[i],train2[i],train4[i],train5[i]])
+    max_train[i] = max([train1[i],train2[i],train4[i],train5[i]])
+    ave_train[i] = average([train1[i],train2[i],train4[i],train5[i]])
 
 
-accu1 = np.append(svhn_node,np.load('accuracy_svhn_node1.npy'))
-accu2 = np.append(svhn_node,np.load('accuracy_svhn_node2.npy'))
-accu3 = np.append(svhn_node,np.load('accuracy_svhn_node3.npy'))
-accu4 = np.append(svhn_node,np.load('accuracy_svhn_node4.npy'))
-accu5 = np.append(svhn_node,np.load('accuracy_svhn_node5.npy'))
+accu1 = np.append(svhn_node,np.load('accuracy_svhn_node_adj3.npy'))
+accu2 = np.append(svhn_node,np.load('accuracy_svhn_node_adj4.npy'))
+accu3 = np.append(svhn_node,np.load('accuracy_svhn_node_adj5.npy'))
+accu4 = np.append(svhn_node,np.load('accuracy_svhn_node_adj7.npy'))
+accu5 = np.append(svhn_node,np.load('accuracy_svhn_node_adj8.npy'))
 
 min_accu1 = np.zeros((length,1))
 max_accu1 = np.zeros((length,1))
 ave_accu1 = np.zeros((length,1))
 
 for i in range(0,length):
-    min_accu1[i] = min([accu1[i],accu2[i],accu3[i],accu4[i],accu5[i]])
-    max_accu1[i] = max([accu1[i],accu2[i],accu3[i],accu4[i],accu5[i]])
-    ave_accu1[i] = average([accu1[i],accu2[i],accu3[i],accu4[i],accu5[i]])
+    min_accu1[i] = min([accu1[i],accu2[i],accu4[i],accu5[i]])
+    max_accu1[i] = max([accu1[i],accu2[i],accu4[i],accu5[i]])
+    ave_accu1[i] = average([accu1[i],accu2[i],accu4[i],accu5[i]])
 
-test1 = np.append(svhn_node1,np.load('test_loss_svhn_node1.npy'))
-test2 = np.append(svhn_node1,np.load('test_loss_svhn_node2.npy'))
-test3 = np.append(svhn_node1,np.load('test_loss_svhn_node3.npy'))
-test4 = np.append(svhn_node1,np.load('test_loss_svhn_node4.npy'))
-test5 = np.append(svhn_node1,np.load('test_loss_svhn_node5.npy'))
+test1 = np.append(svhn_node1,np.load('test_loss_svhn_node_adj3.npy'))
+test2 = np.append(svhn_node1,np.load('test_loss_svhn_node_adj4.npy'))
+test3 = np.append(svhn_node1,np.load('test_loss_svhn_node_adj5.npy'))
+test4 = np.append(svhn_node1,np.load('test_loss_svhn_node_adj7.npy'))
+test5 = np.append(svhn_node1,np.load('test_loss_svhn_node_adj8.npy'))
 
 min_test1 = np.zeros((length,1))
 max_test1 = np.zeros((length,1))
 ave_test1 = np.zeros((length,1))
 
 for i in range(0,length):
-    min_test1[i] = min([test1[i],test2[i],test3[i],test4[i],test5[i]])
-    max_test1[i] = max([test1[i],test2[i],test3[i],test4[i],test5[i]])
-    ave_test1[i] = average([test1[i],test2[i],test3[i],test4[i],test5[i]])
+    min_test1[i] = min([test1[i],test2[i],test4[i],test5[i]])
+    max_test1[i] = max([test1[i],test2[i],test4[i],test5[i]])
+    ave_test1[i] = average([test1[i],test2[i],test4[i],test5[i]])
 
-train1 = np.append(svhn_node1,np.load('train_loss_svhn_node1.npy'))
-train2 = np.append(svhn_node1,np.load('train_loss_svhn_node2.npy'))
-train3 = np.append(svhn_node1,np.load('train_loss_svhn_node3.npy'))
-train4 = np.append(svhn_node1,np.load('train_loss_svhn_node4.npy'))
-train5 = np.append(svhn_node1,np.load('train_loss_svhn_node5.npy'))
+train1 = np.append(svhn_node1,np.load('train_loss_svhn_node_adj3.npy'))
+train2 = np.append(svhn_node1,np.load('train_loss_svhn_node_adj4.npy'))
+train3 = np.append(svhn_node1,np.load('train_loss_svhn_node_adj5.npy'))
+train4 = np.append(svhn_node1,np.load('train_loss_svhn_node_adj7.npy'))
+train5 = np.append(svhn_node1,np.load('train_loss_svhn_node_adj8.npy'))
 
 min_train1 = np.zeros((length,1))
 max_train1 = np.zeros((length,1))
 ave_train1 = np.zeros((length,1))
 
 for i in range(0,length):
-    min_train1[i] = min([train1[i],train2[i],train3[i],train4[i],train5[i]])
-    max_train1[i] = max([train1[i],train2[i],train3[i],train4[i],train5[i]])
-    ave_train1[i] = average([train1[i],train2[i],train3[i],train4[i],train5[i]])
+    min_train1[i] = min([train1[i],train2[i],train4[i],train5[i]])
+    max_train1[i] = max([train1[i],train2[i],train4[i],train5[i]])
+    ave_train1[i] = average([train1[i],train2[i],train4[i],train5[i]])
 
-epochs = np.linspace(0,length-1,length)
+
 
 fig, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9)) = plt.subplots(3,3, sharex=True)
 
@@ -174,7 +175,6 @@ ax3.set_xlabel('Epoch')
 ax3.set_ylabel('Training Loss')
 ax3.fill_between(epochs, min_train1.reshape(length), max_train1.reshape(length), color='plum')
 ax3.legend()
-
 
 accu1 = np.append(cifar_line,np.load('accuracy_cifar_line_adj1.npy'))
 accu2 = np.append(cifar_line,np.load('accuracy_cifar_line_adj2.npy'))
